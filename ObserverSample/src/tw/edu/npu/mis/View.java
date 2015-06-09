dc86dc04a4b08f27db38248b9588f06c01a8955b
woobao <luck9505@yahoo.com.tw>
2015-06-09 16:13:22 +0800

    delete views

--- ObserverSample/src/tw/edu/npu/mis/View.java
+++ ObserverSample/src/tw/edu/npu/mis/View.java
@@ -65,7 +65,7 @@ public class View implements observer, showable{
 
     @Override
     public void update() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
     }
 
 }
